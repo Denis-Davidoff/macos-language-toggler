941c530366f0360c9d2295ec9a829e550b40aceb
DenisDavydov <ddavydov@code.store>
2026-01-30 08:08:50 +0100

    Initial Commit

---
+++ LanguageToggler/LanguageTogglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageTogglerApp.swift
+//  LanguageToggler
+//
+//  Created by Mac on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct LanguageTogglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
